99e979c3d8c86b46f5804549a5f150bdc7ae27a6
Kazi Sabbir Ahmed Opi <ahmedsabbir401@gmail.com>
2023-05-21 23:22:23 +0600

    fardin

--- admin_work.php
+++ admin_work.php
@@ -1,11 +1,59 @@
+<?php
+	include 'nav.php'
+?>
 <!DOCTYPE html>
 <html>
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.rtl.min.css">
+	<script src="https://kit.fontawesome.com/11c476db2a.js" crossorigin="anonymous"></script>
 	<title></title>
 </head>
 <body>
-	<h1>This Ase</h1>
+	<nav>
+		<ul>
+			<li>
+				<a href="#">
+					<img src="img/profile.png">
+					<span class="nav-item">Admin Profile</span>
+				</a>
+			</li>
+			<li><a href="#">
+				<i class="fas fa-home"></i>
+				<span class="nav-item">Home</span>
+			</a></li>
+			<li><a href="#">
+				<i class="fas fa-user"></i>
+				<span class="nav-item">Home</span>
+			</a></li>
+			<li><a href="#">
+				<i class="fas fa-wallet"></i>
+				<span class="nav-item">Wallet</span>
+			</a></li>
+			<li><a href="#">
+				<i class="fas fa-chart-bar"></i>
+				<span class="nav-item">Analytics</span>
+			</a></li>
+			<li><a href="#">
+				<i class="fas fa-tasks"></i>
+				<span class="nav-item">Task</span>
+			</a></li>
+			<li><a href="#">
+				<i class="fas fa-cog"></i>
+				<span class="nav-item">Setting</span>
+			</a></li>
+			<li><a href="#">
+				<i class="fas fa-question-circle"></i>
+				<span class="nav-item">Help</span>
+			</a></li>
+			<li><a href="#">
+				<i class="fas fa-sign-out-alt"></i>
+				<span class="nav-item">Log out</span>
+			</a></li>
+		</ul>
+	</nav>
+
+
 </body>
 </html>
